a47c00e986ac0e6c0fdb804f07c3d092c82d281b
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-09 04:46:42 -0700

    added login page by creating html templates and form class

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 #decarators -modifies the function that follows it
 #@app.route creates an association between the URL given as
@@ -20,3 +21,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+##
+# login view
+##
+@app.route('/login')
+def login():
+    #create form object and passed into render_template.
+    #form on the left is the template and form on the right is created from LoginForm
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
