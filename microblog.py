0a827280d3519e457ca509ccca601620badf1707
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-10 05:46:58 -0700

    added flask shell variables

--- microblog.py
+++ microblog.py
@@ -1,3 +1,8 @@
 #Python script at teh top-level that defines the Flask application instance.
 #'app' is a flask application and is a member of the 'app' application
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
