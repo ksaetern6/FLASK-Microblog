b504b7cd968b19dd89647104de3dcc2baf42d909
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-10 05:09:13 -0700

    added db migrations and user database models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 #Flask reading config file
 #config is the name of module 'config.py' and Config is the actual class
 app.config.from_object(Config)
+##
+# creating db object and migration engine object
+##
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
