e97520c334109b5d512ebad4c0991093fd4102b3
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-15 13:37:17 -0700

    added user profile page using gravatar and two new fields in the db with migration script

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 from flask_login import UserMixin
+from hashlib import md5
 
 ##
 # @type: class
@@ -14,7 +15,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    
+   
+    #User Profile fields 
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     ##
     # @name: set_password
     # @desc: creates a hash of a given string and stores it in the object
@@ -29,6 +33,18 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    ##
+    # @name: avatar
+    # @desc: Uses Gravatar which uses hashes a user's email addr and Gravatar's
+    # URL avatar link to give a profile avatar at a given size using a given 
+    # parameter. Then the method returns the avatar link. If a profile does
+    # not have a link then a default 'identicon' is given.
+    ##
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https:www.gravatar.com/avatar/{}?id=identicon&s={}'.format(
+            digest, size)
+    
     #tells Python how to print objects of this class, for debugging.
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -82,3 +82,17 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+##
+# @name: user
+# @desc: 
+##
+@app.route('/user/<username>') #< > is dynamic for Flask.
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
