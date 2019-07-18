10ee175207904f476537200b57fc78da09034607
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-18 16:18:51 -0700

    Added email handling for environment variables in config

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -22,5 +24,26 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 #view to require users to login to view a page
 login.login_view = 'login'
+
+##
+# ERROR_HANDLING using env variables
+##
+if not app.debug: #if not in debug mode
+    if app.config['MAIL_SERVER']: #if the email server exists
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        #creates SMTPHandler, set to only report errors, not 
+        #warnings/debug messages. Then attached to app.logger
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR) #only errors reported
+        app.logger.addHandler(mail_handler)
 
 from app import routes, models, errors
