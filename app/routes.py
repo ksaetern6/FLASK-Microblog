36f980448e05bfd6d379f4f5b2e573632eec9885
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-20 00:05:10 -0700

    added follower count and following in user.html and follow/unfollow methods in routes.py

--- app/routes.py
+++ app/routes.py
@@ -133,3 +133,40 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile',
                             form=form)
 
+##
+# @name: follow
+# @desc: finds user in db and follows and the current_user follows them.
+##
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+##
+# @name: unfollow
+# @desc: finds username in db and unfollows them from current user.
+##
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
