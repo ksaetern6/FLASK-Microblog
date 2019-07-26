92a9e13e54efe8345cb8359cebeefbf0e0f76f09
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-26 07:36:15 -0700

    changed spacing to comments

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
-#Flask reading config file
-#config is the name of module 'config.py' and Config is the actual class
+# Flask reading config file
+# config is the name of module 'config.py' and Config is the actual class
 app.config.from_object(Config)
 
 ##
@@ -21,48 +21,48 @@ migrate = Migrate(app, db)
 ##
 # using flask_login package 
 ##
-#endpoint for login view
+# endpoint for login view
 login = LoginManager(app)
-#view to require users to login to view a page
+# view to require users to login to view a page
 login.login_view = 'login'
 
 ##
 # ERROR_HANDLING using env variables
 ##
-if not app.debug: #if not in debug mode
-    if app.config['MAIL_SERVER']: #if the email server exists
+if not app.debug:  # if not in debug mode
+    if app.config['MAIL_SERVER']:  # if the email server exists
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
-        #creates SMTPHandler, set to only report errors, not 
-        #warnings/debug messages. Then attached to app.logger
+        # creates SMTPHandler, set to only report errors, not
+        # warnings/debug messages. Then attached to app.logger
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='Microblog Failure',
             credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR) #only errors reported
+        mail_handler.setLevel(logging.ERROR)  # only errors reported
         app.logger.addHandler(mail_handler)
 
 ##
 # LOG FILE HANDLING
 ##
 if not app.debug:
-    #writing/creating log to logs dir
+    # writing/creating log to logs dir
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    #RotatingFileHandler limits log files to 10KB and keeps last 10 as backup
+    # RotatingFileHandler limits log files to 10KB and keeps last 10 as backup
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                        backupCount=10)
-    #logging.Formatter sets the format for the log messages.
-    #includes timestamp, logging level, message, and src file and line number
+                                       backupCount=10)
+    # logging.Formatter sets the format for the log messages.
+    # includes timestamp, logging level, message, and src file and line number
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    #logging level set to INFO category. 
-    #DEBUG > INFO > WARNING > ERROR > CRITICA
+    # logging level set to INFO category.
+    # DEBUG > INFO > WARNING > ERROR > CRITICA
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
