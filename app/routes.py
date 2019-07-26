3382e3041f8363b2e6dd5cc2ddc3cfa351dd96de
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-26 08:50:40 -0700

    added global variable for total listings on a page

--- app/routes.py
+++ app/routes.py
@@ -26,8 +26,15 @@ def index():
         return redirect(url_for('index'))
     # return list of all posts that current_user follows.
     posts = current_user.followed_posts().all()
+
+    # pagination
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+
+    # posts.items is a list of items in the requested page
     return render_template('index.html', title='Home Page', form=form,
-                           posts=posts)
+                           posts=posts.items)
 
 
 ##
@@ -193,5 +200,7 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Explore', posts=posts.items)
--- config.py
+++ config.py
@@ -1,27 +1,32 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#As app needs more configuration items, they can be added to this class.
-#Can also add a subclass for another configuration set
+
+# As app needs more configuration items, they can be added to this class.
+# Can also add a subclass for another configuration set
 class Config(object):
-    #important configuration variable for Flask apps.
-    #Used for cryptographic key, useful to generate signatures or tokens.
-    #SECRET_KEY value is a value sourced from an environment variable.
+    # important configuration variable for Flask apps.
+    # Used for cryptographic key, useful to generate signatures or tokens.
+    # SECRET_KEY value is a value sourced from an environment variable.
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     ##
-    #SQLAlchemy
+    # SQLAlchemy
     # SQLALCHEMY_DATABASE_URI is the location of the database using an
     # enviornment variable
     ##
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    #ERROR_HANDLING EMAIL
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') #server
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25) #port or port 25
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None #enryption
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') #username
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') #password
-    ADMINS = ['kensdevacc@gmail.com'] #emails receiving errors
+
+    # ERROR_HANDLING EMAIL
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')  # server
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)  # port or port 25
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None  # enryption
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # username
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # password
+    ADMINS = ['kensdevacc@gmail.com']  # emails receiving errors
+
+    #Pagination
+    POSTS_PER_PAGE = 3
