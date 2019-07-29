9bd18adfbc097796d4aa643346a2866fbaeae2b2
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-07-29 02:07:05 -0700

    added flask-bootstrap package and remade html templates in bootstrap style

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 # Flask reading config file
@@ -31,6 +33,12 @@ login.login_view = 'login'
 # Flask-Mail
 ##
 mail = Mail(app)
+
+##
+# Flask-Bootstrap
+##
+bootstrap = Bootstrap(app)
+
 
 ##
 # ERROR_HANDLING using env variables
